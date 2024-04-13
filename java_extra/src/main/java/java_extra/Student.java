cf801b8d44e17d1c0e2ae66953b9564b376eacf3
PrathyushaaCode <166895295+PrathyushaaCode@users.noreply.github.com>
2024-04-13 10:45:30 +0530

    Add files via upload

---
+++ java_extra/src/main/java/java_extra/Student.java
@@ -0,0 +1,27 @@
+package java_extra;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+//@Getter
+//@Setter
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@ToString
+//@EqualsAndHashCode
+public class Student {
+	private String name;
+	private int standard;
+	private int age;
+	private int english;
+	private int maths;
+	private int science;
+	private double percentage;
+
+}
